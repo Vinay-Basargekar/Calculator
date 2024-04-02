ceb54a073fa7ba7d17d0607f8fed2e18960e49ba
Guffy-san <vinaybasargekar13@gmail.com>
2024-04-02 05:45:04 +0530

    added calculator structure

---
+++ script.js
@@ -0,0 +1,37 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+let number1,number2,operator;
+
+let operate = (operator,number1,number2) => {
+    if(operator === '+'){
+        return add(number1,number2);
+    }
+    else if(operator === '-'){
+        return subtract(number1,number2);
+    }
+    else if(operator === '*'){
+        return multiply(number1,number2);
+    }
+    else if(operator === '/'){
+        return divide(number1,number2);
+    }
+}   
+let userInput = document.querySelector("input");
+let button = document.querySelector("button");
+
+button.addEventListener("click",() => {})
+
